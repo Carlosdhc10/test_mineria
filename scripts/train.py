a25dd64480551777cb6ca812249f89663129c024
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2025-01-05 11:11:43 -0500

    correccion de ruta

--- scripts/train.py
+++ scripts/train.py
@@ -5,7 +5,7 @@ import torch
 
 def train_model():
     # Ruta a YOLOv5
-    yolov5_dir = "yolov5"
+    yolov5_dir = "../yolov5"
 
     # Comando para ejecutar el entrenamiento con YOLOv5
     os.system(
